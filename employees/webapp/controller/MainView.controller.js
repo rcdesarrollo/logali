136197804e468ab9307c86613392aebacfd344e7
rcdesarrollo <rcdesarrollo@gmail.com>
2021-10-18 22:49:44 +0000

    Elementos y Controladores

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -1,15 +1,42 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup.employees.controller.MainView", {
-			onInit: function () {
+        /*        function myCheck() {
+                    var inputEmployee = this.byId("inputEmployee");
+                    var valueEmploee = inputEmployee.getValue();
+        
+                    if (valueEmploee.length === 6) {
+                        inputEmployee.setDescription("OK");
+                    } else {
+                        inputEmployee.setDescription("Not OK");
+                    }
+                } */
+        // Para controlar los errores
+        var Main = Controller.extend("logaligroup.employees.controller.MainView", {});
 
-			}
-		});
-	});
+        // onValidate: myCheck,
+
+        Main.prototype.onValidate = function () {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmploee = inputEmployee.getValue();
+
+            if (valueEmploee.length === 6) {
+                //inputEmployee.setDescription("OK");
+                this.byId("labelCountry").setVisible(true);
+                this.byId("slCountry").setVisible(true);
+            } else {
+                //inputEmployee.setDescription("Not OK");
+                this.byId("labelCountry").setVisible(false);
+                this.byId("slCountry").setVisible(false);
+            }
+        };
+
+        return Main;
+
+    });
