9e66f323ab6d51ab34b19e05a3345896cd8f115f
rcdesarrollo <rcdesarrollo@gmail.com>
2021-11-06 11:08:54 +0000

    Navegacion

--- employees/webapp/controller/EmployeeDetails.controller.js
+++ employees/webapp/controller/EmployeeDetails.controller.js
@@ -11,62 +11,62 @@ sap.ui.define([
     };
 
     function onCreateIncidence() {
-        
+
         var tableIncidence = this.getView().byId("tableIncidence");
-        var newIncidence = sap.ui.xmlfragment("logaligroup.employees.fragment.NewIncidence",this);
+        var newIncidence = sap.ui.xmlfragment("logaligroup.employees.fragment.NewIncidence", this);
         var incidenceModel = this.getView().getModel("incidenceModel");
         var odata = incidenceModel.getData();
         var index = odata.length;
-        
-        odata.push({ index : index + 1, _ValidateDate: false, EnabledSave : false });
+
+        odata.push({ index: index + 1, _ValidateDate: false, EnabledSave: false });
         incidenceModel.refresh();
 
         newIncidence.bindElement("incidenceModel>/" + index);
         tableIncidence.addContent(newIncidence);
-        
+
     };
 
-    function onDeleteIncidence(oEvent){
-                
-        var contextObj = oEvent.getSource().getBindingContext("incidenceModel").getObject();
-        
-        MessageBox.confirm( this.getView().getModel("i18n").getResourceBundle().getText("confirmDeleteIncidence"),{
+    function onDeleteIncidence(oEvent) {
 
-            onClose : function(oAction){
-                if(oAction === "OK"){
-                    this._bus.publish("incidence","onDeleteIncidence", { 
-                            IncidenceId :contextObj.IncidenceId,
-                            SapId : contextObj.SapId,
-                            EmployeeId : contextObj.EmployeeId
-                    });  
+        var contextObj = oEvent.getSource().getBindingContext("incidenceModel").getObject();
+
+        MessageBox.confirm(this.getView().getModel("i18n").getResourceBundle().getText("confirmDeleteIncidence"), {
+
+            onClose: function (oAction) {
+                if (oAction === "OK") {
+                    this._bus.publish("incidence", "onDeleteIncidence", {
+                        IncidenceId: contextObj.IncidenceId,
+                        SapId: contextObj.SapId,
+                        EmployeeId: contextObj.EmployeeId
+                    });
                 };
             }.bind(this)
-        });    
+        });
 
     };
 
-    function onSaveIncidence(oEvent){ 
+    function onSaveIncidence(oEvent) {
         var incidence = oEvent.getSource().getParent().getParent();
         var incidenceRow = incidence.getBindingContext("incidenceModel");
 
-        this._bus.publish("incidence","onSaveIncidence", { incidenceRow : incidenceRow.sPath.replace('/','') });
+        this._bus.publish("incidence", "onSaveIncidence", { incidenceRow: incidenceRow.sPath.replace('/', '') });
 
 
     };
 
-    function updateIncidenceCreationDate(oEvent){
+    function updateIncidenceCreationDate(oEvent) {
         let context = oEvent.getSource().getBindingContext("incidenceModel");
         let contextObj = context.getObject();
         let oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
 
         // getValue = obtener el valo
-        if(!oEvent.getSource().isValidValue()){
+        if (!oEvent.getSource().isValidValue()) {
             contextObj._ValidateDate = false;
             contextObj.CreationDateState = "Error";
-            MessageBox.error( oResourceBundle.getText("errorCreationDateValue") ,{
+            MessageBox.error(oResourceBundle.getText("errorCreationDateValue"), {
                 title: "Error",
                 onClose: null,
-                styleClass : "",
+                styleClass: "",
                 actions: MessageBox.Action.Close,
                 emphasizedAction: null,
                 initialFocus: null,
@@ -78,40 +78,40 @@ sap.ui.define([
             contextObj.CreationDateState = "None";
         };
 
-        if(oEvent.getSource().isValidValue() && contextObj.Reason){
+        if (oEvent.getSource().isValidValue() && contextObj.Reason) {
             contextObj.EnabledSave = true;
         } else {
             contextObj.EnabledSave = false;
         };
-        
+
         context.getModel().refresh();
     };
-    
-    function updateIncidenceReason(oEvent){
+
+    function updateIncidenceReason(oEvent) {
         let context = oEvent.getSource().getBindingContext("incidenceModel");
         let contextObj = context.getObject();
 
         if (oEvent.getSource().getValue()) {
             contextObj.ReasonX = true;
-            contextObj.ReasonState = "None";                        
+            contextObj.ReasonState = "None";
         } else {
             contextObj.ReasonState = "Error";
         };
 
-        if(contextObj._ValidateDate && oEvent.getSource().getValue() ){
+        if (contextObj._ValidateDate && oEvent.getSource().getValue()) {
             contextObj.EnabledSave = true;
         } else {
             contextObj.EnabledSave = false;
         };
-        
+
         context.getModel().refresh();
     };
 
-    function updateIncidenceType(oEvent){
+    function updateIncidenceType(oEvent) {
         let context = oEvent.getSource().getBindingContext("incidenceModel");
         let contextObj = context.getObject();
 
-        if(contextObj._ValidateDate && contextObj.Reason ){
+        if (contextObj._ValidateDate && contextObj.Reason) {
             contextObj.EnabledSave = true;
         } else {
             contextObj.EnabledSave = false;
@@ -122,19 +122,27 @@ sap.ui.define([
         context.getModel().refresh();
     };
 
+    function toOrderDetails(oEvent) {
+        var orderID = oEvent.getSource().getBindingContext("odataNorthwind").getObject().OrderID;
+        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        oRouter.navTo("RouteOrderDetails", {
+            OrderID: orderID
+        });
+    };
 
     var EmployeeDetails = Controller.extend("logaligroup.employees.controller.EmployeeDetails", {});
-    
+
     EmployeeDetails.prototype.onInit = onInit;
     EmployeeDetails.prototype.onCreateIncidence = onCreateIncidence;
     EmployeeDetails.prototype.onDeleteIncidence = onDeleteIncidence;
-    EmployeeDetails.prototype.onSaveIncidence=onSaveIncidence;
-    EmployeeDetails.prototype.updateIncidenceCreationDate=updateIncidenceCreationDate;
-    EmployeeDetails.prototype.updateIncidenceReason=updateIncidenceReason;
-    EmployeeDetails.prototype.updateIncidenceType=updateIncidenceType;
+    EmployeeDetails.prototype.onSaveIncidence = onSaveIncidence;
+    EmployeeDetails.prototype.updateIncidenceCreationDate = updateIncidenceCreationDate;
+    EmployeeDetails.prototype.updateIncidenceReason = updateIncidenceReason;
+    EmployeeDetails.prototype.updateIncidenceType = updateIncidenceType;
+    EmployeeDetails.prototype.toOrderDetails = toOrderDetails;
 
     EmployeeDetails.prototype.Formatter = formatter;
-    
+
 
     return EmployeeDetails;
 
